dbe2d302122552634efad610e0c8b565055291fa
nondefyde <nondefyde@gmail.com>
2018-05-31 16:05:38 +0100

    clean up

--- src/lang/en.js
+++ src/lang/en.js
@@ -3,11 +3,8 @@ export default {
 		server: 'Error in server interaction',
 		resource_not_found: 'Resource not found!',
 		inputs: 'There are problems with your input',
-		not_auth_token: 'No authorization token provided',
 		not_found: 'Resource not found',
 		no_update_input: 'Nothing to update',
-		no_file_uploaded: 'No file uploaded',
-		nothing_to_save: `There's nothing to save`,
 	},
 	report: {
 	},
